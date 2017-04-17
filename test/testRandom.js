cfeb54cf30984d5f7e4fe155eb8d00363f851bbd
Edvinas <edvinasrp@gmail.com>
2017-04-17 18:18:49 +0100

    added a distribution test

--- test/testRandom.js
+++ test/testRandom.js
@@ -3,7 +3,7 @@ const Random = require('../random');
 
 describe('Random', () => {
     describe('value()', () => {
-        it('generates a number between 0 inclusive and 1 exclusive.', () => {
+        it('generates a number between 0 inclusive and 1 exclusive', () => {
             const value = Random.value();
 
             assert.isTrue(0 <= value, 'should be more than or equal to 0');
@@ -12,11 +12,33 @@ describe('Random', () => {
     });
 
     describe('range(min, max)', () => {
-        it('generates a number within the given range.', () => {
+        it('generates a number within the given range', () => {
             const value = Random.range(10, 100);
 
             assert.isTrue(10 <= value, 'should be more than or equal to 0');
-            assert.isTrue(100 >= value, 'should be less than or equal to 100');
+            assert.isTrue(100 > value, 'should be less than or equal to 100');
+        });
+
+        it('generates a number including min value and excluding max value', () => {
+            assert.equal(0, Random.range(0, 1));
+        });
+
+        it('distributes values evenly', () => {
+            const occurances = new Array(101);
+
+            /** Generate a large array of values. */
+            const values = new Uint32Array(10000).map((value) => {
+                return Random.range(0, 100);
+            });
+
+            /** Count how many times does each value in the array occur. */
+            values.forEach((value) => {
+                occurances[value] = (occurances[value] || 0) + 1;
+            });
+
+            occurances.forEach((value, index) => {
+                assert.approximately(value / values.length, 100 / values.length, 0.01);
+            });
         });
     });
 });
