418860eb345c6ad245f3ee6fe303e10aea019ecb
SkepticalHippo <edvinasrp@gmail.com>
2022-08-09 20:25:56 +0100

    Convert test suite to jest

--- random.test.js
+++ random.test.js
@@ -1,13 +1,12 @@
-const assert = require('chai').assert;
-const Random = require('../random');
+const Random = require('./random');
 
 describe('Random', () => {
     describe('value()', () => {
         it('generates a number between 0 inclusive and 1 exclusive', () => {
             const value = Random.value();
 
-            assert.isTrue(0 <= value, 'should be more than or equal to 0');
-            assert.isTrue(1 > value, 'should be less than 1');
+            expect(value).toBeGreaterThanOrEqual(0);
+            expect(value).toBeLessThan(1);
         });
     });
 
@@ -15,12 +14,12 @@ describe('Random', () => {
         it('generates a number within the given range', () => {
             const value = Random.range(10, 100);
 
-            assert.isTrue(10 <= value, 'should be more than or equal to 0');
-            assert.isTrue(100 > value, 'should be less than or equal to 100');
+            expect(value).toBeGreaterThanOrEqual(10);
+            expect(value).toBeLessThan(100);
         });
 
         it('generates a number including min value and excluding max value', () => {
-            assert.equal(0, Random.range(0, 1));
+            expect(Random.range(0, 1)).toEqual(0);
         });
 
         it('distributes values evenly', () => {
@@ -44,11 +43,11 @@ describe('Random', () => {
 
             /** Contains each of the values at least once. */
             range.forEach((value) => {
-                assert.isTrue(values.includes(value), 'should contain '+value);
+                expect(values.includes(value)).toBeTruthy();
             });
 
-            occurances.forEach((value, index) => {
-                assert.approximately(value / values.length, ratio, ratio / 2);
+            occurances.forEach((value) => {
+                expect(value / values.length).toBeCloseTo(ratio, 2);
             });
         });
     });
